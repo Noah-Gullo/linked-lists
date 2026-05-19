20ef3313275d080fe13151ce1c294f8a2321729c
Noah Gullo <noahgulloingram@gmail.com>
2026-05-18 20:38:57 -0700

    feat: first pass at append()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 class LinkedList{
+    list;
     constructor(){
         this.list = null;
+    }
+    
+    append = function(value){
+        if(this.list === null){
+            this.list = new Node(value, null);
+            return;
+        }else{
+            let listCopy = this.list;
+            while(listCopy.nextNode != null){
+                listCopy = listCopy.nextNode;
+            }
+            
+            listCopy.nextNode = new Node(value, null);
+        }
     }
 }
 
 class Node{
-    constructor(){
-        this.value = null;
-        this.nextNode = null;
+    value = null;
+    nextNode = null;
+
+    constructor(value, nextNode){
+        this.value = value;
+        this.nextNode = nextNode;
     }
 }
 
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("mouse");
+
+console.log(list);
