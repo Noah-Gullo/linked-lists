c2befa0921f332007fad92d74665eb2231b2b3e9
Noah Gullo <noahgulloingram@gmail.com>
2026-05-18 20:13:24 -0700

    feat: basic file setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import "./styles.css";
+class LinkedList{
+    constructor(){
+        this.list = null;
+    }
+}
+
+class Node{
+    constructor(){
+        this.value = null;
+        this.nextNode = null;
+    }
+}
+
